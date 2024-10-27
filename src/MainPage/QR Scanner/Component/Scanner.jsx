11fa1c480eaef56e7423f107a1e2cb4dc19edbdb
Oluwapelumielisha <elishaoluwapelumi38@gmail.com>
2024-10-27 11:07:50 -0700

    fixing camera

--- src/MainPage/QR Scanner/Component/Scanner.jsx
+++ src/MainPage/QR Scanner/Component/Scanner.jsx
@@ -5,22 +5,13 @@ import { useScanner } from "../hook/useScanner";
 const Scanner = () => {
   const { handleScan, handleError, scanResult } = useScanner();
 
-  // State to manage camera facing mode
-  const [facingMode, setFacingMode] = useState('environment'); // Start with back camera
-  const [key, setKey] = useState(0); // Force re-render on toggle
-
-  // Function to toggle camera facing mode
-  const toggleCamera = () => {
-    setFacingMode((prevMode) => (prevMode === 'environment' ? 'user' : 'environment'));
-    setKey(prevKey => prevKey + 1); // Increment key to force component update
-  };
 
   return (
     <div className="h-screen bg-customColor overflow-auto">
-      <div className="flex items-center justify-center pt-10">
-        <div className="flex items-center justify-center w-60 h-60 rounded-lg border-2 border-orange-400 bg-gray-800">
+      <div className="flex items-center justify-center pt-20">
+        <div className="flex items-center justify-center w-90 h-90 rounded-lg border-2 border-orange-400 bg-gray-800">
         <QrReader
-          delay={300}
+          delay={100}
           style={{ width: '100%', height: '100%' }}
           onError={handleError}
           onScan={handleScan}
@@ -31,21 +22,17 @@ const Scanner = () => {
         </div>
       </div>
       <div className="flex items-center justify-center pt-10">
-        <h3 className="text-white text-xl">Scan Result: &nbsp; &nbsp; </h3>
+        <h3 className="text-white text-xl">Scan Result: &nbsp;</h3>
         {scanResult?.text ? (
           <p className="text-green-500">{scanResult.text}</p>
         ) : (
           <p className="text-white text-xl">No QR code detected yet</p>
         )}
       </div>
-      <div className="flex items-center justify-center pt-10">
-        <button 
-          className="bg-orange-500 text-white px-4 py-2 rounded"
-          onClick={toggleCamera}
-        >
-          Switch Camera
-        </button>
+      <div className="flex items-center justify-center mt-12 mb-12">
+        <p className="opacity-0 mt-8 text-white text-sm">Loading...</p>
       </div>
+    
     </div>
   );
 };
