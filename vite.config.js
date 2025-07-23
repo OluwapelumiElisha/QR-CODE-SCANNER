d69d841a346f2277a2c1cbc0b58845397ab0eb4b
OluwapelumiElisha <elishaoluwapelumi38@gmail.com>
2025-07-23 17:16:59 +0100

    Updates

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,54 @@
 // });
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { VitePWA } from 'vite-plugin-pwa';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       manifest: {
+//         name: 'Youth Camp Anniversary 2025',
+//         short_name: 'YouthCamp',
+//         start_url: '/',
+//         display: 'standalone',
+//         theme_color: '#ffffff',
+//         background_color: '#ffffff',
+//         icons: [
+//           {
+//             src: '/web-app-manifest-192x192.png',
+//             sizes: '192x192',
+//             type: 'image/png',
+//           },
+//           {
+//             src: '/web-app-manifest-512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png',
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// });
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: './', // ✅ This line is key for Vercel static deployment
   plugins: [
     react(),
     VitePWA({
@@ -58,7 +100,5 @@ export default defineConfig({
     },
   },
 });
-
-
 
 
